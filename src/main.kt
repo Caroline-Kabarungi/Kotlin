0a608203b0a45fae3f3f36fd09d609a2b9e6f152
Caroline-Kabarungi <josephkabarungicaroline@gmail.com>
2022-04-14 09:26:49 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,45 @@
+fun main(){
+    var cash=currentAccount(2455,"caroline",3400)
+    var money=deposit(3.545)
+    var amount=withdraw(2.567)
+    var sentence=details()
+    var list=("banana")
+    println(list.indexOf(0,2,4))
+
+
+
+}
+class currentAccount(accountnumber:Int,accountname:String,balance:Int){
+
+}
+fun deposit(amount:Double){
+
+}
+
+fun withdraw(amount:Double){
+
+}
+fun details(){
+
+}
+class SavingAccount(accountnumber:Int,accountname:String,balance:Int){
+
+}
+//question 3
+ class Items(name:String,Weight:Int,Price:Int,category:String){
+ }
+  if (Item is String)
+      println(name)
+} else{
+    //println(Int)
+}
+//question 4
+fun name(b:String):String{
+    var list=("banana")
+    return (list)
+
+
+}
+
+
+
